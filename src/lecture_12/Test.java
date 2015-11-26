12f8d171da3ea1c512c1017a5bc51464e1ce7e8d
alex-icez <alex-icex@mail.ru>
2015-11-26 18:44:52 +0300

    mess

--- src/lecture_12/Test.java
+++ src/lecture_12/Test.java
@@ -4,7 +4,10 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 class MyFrame extends JFrame {
-	
+	public MyFrame() {
+		this.setTitle("Окно в мир!!!");
+		this.setSize(640, 480);
+	}
 }
 
 public class Test {
