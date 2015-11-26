0f56f77a1d61633dfc81dd2de25a78c1b428d874
alex-icez <alex-icex@mail.ru>
2015-11-26 19:54:20 +0300

    mess

---
+++ src/lecture_12/Sample1.java
@@ -0,0 +1,16 @@
+package lecture_12;
+
+
+class Frame extends JFrame {
+	
+	
+}
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
