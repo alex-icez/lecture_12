6c2d8a0c7a9afda66fe39c58c8df2f34970f4066
alex-icez <alex-icex@mail.ru>
2015-11-26 19:32:13 +0300

    mess

--- src/lecture_12/Test.java
+++ src/lecture_12/Test.java
@@ -16,7 +16,7 @@ class MyPanel extends JPanel {
 		g.setColor(new Color(getWidth() % 256, getWidth() % 256, getHeight() % 256));
 		g.fillRect(10, 10, getWidth() / 4, getHeight() / 4);
 		g.drawString("Hello World", getWidth() / 4, getHeight() / 4);	
-		//g.drawOval(x, y, width, height);
+		
 	}
 }
 
@@ -29,6 +29,7 @@ class MyFrame extends JFrame {
 		panel = new MyPanel();
 		add(panel);
 		setVisible(true);
+		repaint();
 	}
 }
 
