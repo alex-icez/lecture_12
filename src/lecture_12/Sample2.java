7a0cbe862c3b22ada0d374044ae3b13cc2546051
alex-icez <alex-icex@mail.ru>
2015-11-26 20:17:11 +0300

    mess

--- src/lecture_12/Sample2.java
+++ src/lecture_12/Sample2.java
@@ -1,13 +1,21 @@
 package lecture_12;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
 
 
 
 class Gnome extends JFrame {
+	public JLabel label1;
+	
 	public Gnome() {
 		setTitle("Gnome");
 		setSize(640, 480);
+		setLayout(new FlowLayout());
+		
+		
+		
 		
 		
 		setVisible(true);
