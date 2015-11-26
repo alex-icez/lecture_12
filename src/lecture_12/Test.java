6e2362f84a1a4a3453550984fe7be14de4ab85ab
alex-icez <alex-icex@mail.ru>
2015-11-26 18:51:59 +0300

    mess

--- src/lecture_12/Test.java
+++ src/lecture_12/Test.java
@@ -16,11 +16,10 @@ public class Test {
 	public static void main(String[] args) {
 		//SwingUtilities.invokeLater(() -> new MyFrame());
 		SwingUtilities.invokeLater(new Runnable() {
-			@Override
 			public void run() {
 				new MyFrame();
 			}
-		};
+		});
 		
 		
 	}
