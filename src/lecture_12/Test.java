cc1f2e93d4dbcc3d315bf0e0b76ef5d9cdafc554
alex-icez <alex-icex@mail.ru>
2015-11-26 19:34:25 +0300

    mess

--- src/lecture_12/Test.java
+++ src/lecture_12/Test.java
@@ -17,7 +17,7 @@ class MyPanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponents(g);
 		Graphics2D g2 = (Graphics2D)g;
-		g2.setStroke( new BasicStroke(1));
+		g2.setStroke( new BasicStroke(4));
 		g.setColor(new Color(getWidth() % 256, getWidth() % 256, getHeight() % 256));
 		g.fillRect(10, 10, getWidth() / 4, getHeight() / 4);
 		g.drawString("Hello World", getWidth() / 4, getHeight() / 4);	
