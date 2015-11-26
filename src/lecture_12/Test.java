30fe7b8a2e79f65c0e80a573b7c20c2b1a47c968
alex-icez <alex-icex@mail.ru>
2015-11-26 18:43:16 +0300

    mess

--- src/lecture_12/Test.java
+++ src/lecture_12/Test.java
@@ -1,10 +1,14 @@
 package lecture_12;
 
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+class MyFrame extends JFrame {
+	
+}
+
 public class Test {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
