cfb94ddf7d48271699673397f8656a6d15a00379
alex-icez <alex-icex@mail.ru>
2015-11-26 19:57:02 +0300

    mess

--- src/lecture_12/Sample1.java
+++ src/lecture_12/Sample1.java
@@ -1,11 +1,17 @@
 package lecture_12;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
 class Panel extends JPanel {
 	int x, y;
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawString("(" + x + ", " + y + ")", x, y);
+	}
 }
 
 class Frame extends JFrame {
