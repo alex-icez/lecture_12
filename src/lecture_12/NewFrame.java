a5db83b20463b550b620804577de12d279f865c9
alex-icez <alex-icex@mail.ru>
2015-11-26 20:47:34 +0300

    mess

--- src/lecture_12/NewFrame.java
+++ src/lecture_12/NewFrame.java
@@ -12,6 +12,7 @@ import java.awt.GridBagConstraints;
 import javax.swing.JSpinner;
 import java.awt.Insets;
 import javax.swing.JTable;
+import javax.swing.JRadioButton;
 
 public class NewFrame extends JFrame {
 
@@ -44,31 +45,30 @@ public class NewFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JSpinner spinner = new JSpinner();
 		GridBagConstraints gbc_spinner = new GridBagConstraints();
 		gbc_spinner.insets = new Insets(0, 0, 5, 5);
-		gbc_spinner.gridx = 5;
+		gbc_spinner.gridx = 6;
 		gbc_spinner.gridy = 1;
 		contentPane.add(spinner, gbc_spinner);
 		
-		JButton btnNewButton = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.fill = GridBagConstraints.VERTICAL;
-		gbc_btnNewButton.gridx = 2;
-		gbc_btnNewButton.gridy = 2;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		GridBagConstraints gbc_rdbtnNewRadioButton = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton.insets = new Insets(0, 0, 0, 5);
+		gbc_rdbtnNewRadioButton.gridx = 3;
+		gbc_rdbtnNewRadioButton.gridy = 3;
+		contentPane.add(rdbtnNewRadioButton, gbc_rdbtnNewRadioButton);
 		
 		table = new JTable();
 		GridBagConstraints gbc_table = new GridBagConstraints();
 		gbc_table.fill = GridBagConstraints.BOTH;
-		gbc_table.gridx = 7;
+		gbc_table.gridx = 8;
 		gbc_table.gridy = 3;
 		contentPane.add(table, gbc_table);
 	}
