45b9563922cf6310b97ea70994ed21634afcaad3
alex-icez <alex-icex@mail.ru>
2015-11-26 18:51:18 +0300

    mess

--- src/lecture_12/Test.java
+++ src/lecture_12/Test.java
@@ -14,6 +14,14 @@ class MyFrame extends JFrame {
 
 public class Test {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> new MyFrame());
+		//SwingUtilities.invokeLater(() -> new MyFrame());
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new MyFrame();
+			}
+		};
+		
+		
 	}
 }
